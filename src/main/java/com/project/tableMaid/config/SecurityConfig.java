d077891dd205073ede9dc244046e339d5f3000d4
donasman <rjsgmlq33@gmail.com>
2024-05-28 21:16:26 +0900

    Feat: BE 배포

---
+++ src/main/java/com/project/tableMaid/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.project.tableMaid.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
+        http.csrf().disable();
+        http.authorizeHttpRequests()
+                .antMatchers("/server/hc", "/server/env")
+                .permitAll()
+                .antMatchers("/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+    }
+}
---
+++ src/main/java/com/project/tableMaid/controller/HealthCheckController.java
@@ -0,0 +1,32 @@
+package com.project.tableMaid.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/server")
+public class HealthCheckController {
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.name}")
+    private String serverName;
+    @Value("${server.deploy-address}")
+    private String deployAddress;
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/hc")
+    public ResponseEntity<?> healthCheck() {
+        return ResponseEntity.ok(Map.of("serverName", serverName, "deployAddress",deployAddress, "port", port));
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}

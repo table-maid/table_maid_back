2f911f435ec732ed8ef2c3f847d8be8bf2123c57
wjdcks2252 <wjdcks2252@naver.com>
2024-05-28 14:36:03 +0900

    feat:order, sales dto,entity 완료

---
+++ src/main/java/com/project/tableMaid/service/SalesService.java
@@ -0,0 +1,13 @@
+package com.project.tableMaid.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SalesService {
+
+    //판매 POST
+    public void insertSales() {
+
+    }
+
+}

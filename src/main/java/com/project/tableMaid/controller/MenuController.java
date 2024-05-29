dc43e9c4d7ed9e1cc28765f6998183d7e2bc5d53
leesongchi <kyoti0414@naver.com>
2024-05-29 13:37:11 +0900

    Revert "Reapply "Feat: 옵션등록""
    This reverts commit 856902324953cd3d2a2fb139857d1c8903e4bb41.

--- src/main/java/com/project/tableMaid/controller/MenuController.java
+++ src/main/java/com/project/tableMaid/controller/MenuController.java
@@ -1,8 +1,6 @@
 package com.project.tableMaid.controller;
 
 import com.project.tableMaid.dto.menu.request.AddMenuCategoryReqDto;
-import com.project.tableMaid.dto.menu.request.AddOptionNameReqDto;
-import com.project.tableMaid.dto.menu.request.AddOptionTitleReqDto;
 import com.project.tableMaid.dto.menu.request.RegisterMenuReqDto;
 import com.project.tableMaid.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,18 +26,6 @@ public class MenuController {
     @PostMapping("/add/category")
     public ResponseEntity<?> addMenuCategory(@RequestBody AddMenuCategoryReqDto addMenuCategoryReqDto) {
         menuService.insertMenuCategory(addMenuCategoryReqDto);
-        return ResponseEntity.created(null).body(true);
-    }
-
-    @PostMapping("/add/title")
-    public ResponseEntity<?> addOptionTitle(@RequestBody AddOptionTitleReqDto addOptionTitleReqDto) {
-        menuService.insertOptionTitle(addOptionTitleReqDto);
-        return ResponseEntity.created(null).body(true);
-    }
-
-    @PostMapping("/add/name")
-    public ResponseEntity<?> addOptionName(@RequestBody AddOptionNameReqDto addOptionNameReqDto) {
-        menuService.insertOptionName(addOptionNameReqDto);
         return ResponseEntity.created(null).body(true);
     }
 }

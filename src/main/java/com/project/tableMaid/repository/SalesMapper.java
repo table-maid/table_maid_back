7e06bd11fa114d222ecb6ab8d58dcdaa6fb5f0e0
ez0603 <dlwldjs3132@naver.com>
2024-06-08 20:36:24 +0900

    Fix : 메뉴 매출 수정 완료

--- src/main/java/com/project/tableMaid/repository/SalesMapper.java
+++ src/main/java/com/project/tableMaid/repository/SalesMapper.java
@@ -14,7 +14,17 @@ public interface SalesMapper {
     public int saveOrders(List<Order> orders);
     public List<TotalSales> findTotalSales();
     public List<Sales> findSelectDaySales();
-    public List<Sales> findMenuTotalSales();
+    public List<Sales> findMenuTotalSales(
+            @Param("adminId") int adminId,
+            @Param("menuId") int menuId,
+            @Param("menuName") String menuName,
+            @Param("year") int year,
+            @Param("month") int month,
+            @Param("day") int day,
+            @Param("count") int count,
+            @Param("menuTotalSales") int menuTotalSales,
+            @Param("menuImgUrl") String menuImgUrl
+    );
     public List<Order> findOrders();
     public int deleteSales(@Param("orderNumber") int orderNumber, @Param("adminId") int adminId);
     public int deleteOrder(@Param("orderNumber") int orderNumber, @Param("adminId") int adminId);
